a2838c2a68b936b92c14063a3bc610e007923149
Guillaume Viguier <guillaume@viguierjust.com>
2015-09-17 15:16:50 +0200

    Added fontawesome

--- template.php
+++ template.php
@@ -3,3 +3,7 @@
  * @file
  * template.php
  */
+
+function gvj_preprocess_html(&$variables) {
+  drupal_add_css('https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css', array('type' => 'external'));
+}
