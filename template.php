12362f71882590e859fccd6d398b4167baea886d
Guillaume Viguier <guillaume@viguierjust.com>
2015-09-17 17:12:14 +0200

    Changed footer region

--- template.php
+++ template.php
@@ -7,3 +7,9 @@
 function gvj_preprocess_html(&$variables) {
   drupal_add_css('https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css', array('type' => 'external'));
 }
+
+function gvj_preprocess_region(&$variables, $hook) {
+  if($variables['region'] == "footer"){
+    $variables['classes_array'][] = 'container';
+  }
+}
