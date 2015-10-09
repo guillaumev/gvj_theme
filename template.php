b9426dfa80416857320ebcad39a1187ca668a102
Guillaume Viguier <guillaume@viguierjust.com>
2015-10-09 16:45:03 +0200

    Change readmore

--- template.php
+++ template.php
@@ -70,7 +70,6 @@ function gvj_preprocess_node(&$variables) {
     $readmore = $variables['content']['links']['node']['#links']['node-readmore'];
     $readmore['title'] = t('Read more &raquo;');
     $readmore['attributes']['class'] = array('btn', 'btn-default');
-    debug($readmore);
 
     // remove the old link
     unset($variables['content']['links']['node']['#links']['node-readmore']);
